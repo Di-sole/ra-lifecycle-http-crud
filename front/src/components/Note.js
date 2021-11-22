9ef1d43dcbbe5e8bfce0b3576774a54efcd6685e
Olga Abramovich <disole@bk.ru>
2021-11-22 20:05:36 +0500

    first commit

---
+++ front/src/components/Note.js
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+
+function Note({ note, onDelete }) {
+	return (
+		<div className="note">
+			<p className="note-content">{note.content}</p>
+			<button className="btn delete-btn" onClick={() => onDelete(note.id)}>&#10060;</button>
+		</div>
+	)
+}
+
+Note.propTypes = {
+	note: PropTypes.shape({
+		id: PropTypes.string,
+		content: PropTypes.string
+	}),
+	onDelete: PropTypes.func
+}
+
+export default Note;
